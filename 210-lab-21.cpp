770422f7f15ed591d2842be3ecab6e290585b555
gillianrhett <gillianrhett@gmail.com>
2026-03-14 16:58:29 -0700

    working on Goat class

--- 210-lab-21.cpp
+++ 210-lab-21.cpp
@@ -1,113 +1,144 @@
+// COMSC-210 | Lab 21 | Gillian Rhett
+
 #include <iostream>
+#include <array>
 using namespace std;
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20; // constants from the original file
+const int NAMES_SIZE = 15, COLORS_SIZE = 15, AGE_MIN = 0, AGE_MAX = 20; // my new constants for Goat class
 class DoublyLinkedList {
-private:
-struct Node {
-    int data;
-    Node* prev;
-    Node* next;
-    Node(int val, Node* p = nullptr, Node* n = nullptr) {
-        data = val;
-        prev = p;
-        next = n;
-    }
-};
-Node* head;
-Node* tail;
-public:
-// constructor
-DoublyLinkedList() { head = nullptr; tail = nullptr; }
-void push_back(int value) {
-    Node* newNode = new Node(value);
-    if (!tail) // if there's no tail, the list is empty
-    head = tail = newNode;
-    else {
-    tail->next = newNode;
-    newNode->prev = tail;
-    tail = newNode;
-    }
-}
-void push_front(int value) {
-    Node* newNode = new Node(value);
-    if (!head) // if there's no head, the list is empty
-    head = tail = newNode;
-    else {
-    newNode->next = head;
-    head->prev = newNode;
-    head = newNode;
-    }
-}
-void insert_after(int value, int position) {
-    if (position < 0) {
-    cout << "Position must be >= 0." << endl;
-    return;
-    }
-    Node* newNode = new Node(value);
-    if (!head) {
+    private:
+    struct Node {
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+            data = val;
+            prev = p;
+            next = n;
+        }
+    };
+    Node* head;
+    Node* tail;
+    public:
+    // constructor
+    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!tail) // if there's no tail, the list is empty
         head = tail = newNode;
+        else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+        }
+    }
+    void push_front(int value) {
+        Node* newNode = new Node(value);
+        if (!head) // if there's no head, the list is empty
+        head = tail = newNode;
+        else {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        }
+    }
+    void insert_after(int value, int position) {
+        if (position < 0) {
+        cout << "Position must be >= 0." << endl;
         return;
-    }
-    Node* temp = head;
-    for (int i = 0; i < position && temp; ++i)
-    temp = temp->next;
-    if (!temp) {
-        cout << "Position exceeds list size. Node not inserted.\n";
-        delete newNode;
-        return;
-    }
-    newNode->next = temp->next;
-    newNode->prev = temp;
-    if (temp->next)
-        temp->next->prev = newNode;
-    else
-        tail = newNode; // Inserting at the end
-    temp->next = newNode;
-}
-void delete_node(int value) {
-    if (!head) return; // Empty list
-    Node* temp = head;
-    while (temp && temp->data != value)
-    temp = temp->next;
-    if (!temp) return; // Value not found
-    if (temp->prev) {
-        temp->prev->next = temp->next;
-    } else {
-        head = temp->next; // Deleting the head
-    }
-    if (temp->next) {
-        temp->next->prev = temp->prev;
-    } else {
-        tail = temp->prev; // Deleting the tail
-    }
-    delete temp;
-}
-void print() {
-    Node* current = head;
-    if (!current) return;
-        while (current) {
-        cout << current->data << " ";
-        current = current->next;
-    }
-    cout << endl;
-}
-void print_reverse() {
-    Node* current = tail;
-    if (!current) return;
-    while (current) {
-        cout << current->data << " ";
-        current = current->prev;
-    }
-    cout << endl;
-}
-~DoublyLinkedList() {
-    while (head) {
+        }
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+            return;
+        }
         Node* temp = head;
-        head = head->next;
+        for (int i = 0; i < position && temp; ++i)
+        temp = temp->next;
+        if (!temp) {
+            cout << "Position exceeds list size. Node not inserted.\n";
+            delete newNode;
+            return;
+        }
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        if (temp->next)
+            temp->next->prev = newNode;
+        else
+            tail = newNode; // Inserting at the end
+        temp->next = newNode;
+    }
+    void delete_node(int value) {
+        if (!head) return; // Empty list
+        Node* temp = head;
+        while (temp && temp->data != value)
+        temp = temp->next;
+        if (!temp) return; // Value not found
+        if (temp->prev) {
+            temp->prev->next = temp->next;
+        } else {
+            head = temp->next; // Deleting the head
+        }
+        if (temp->next) {
+            temp->next->prev = temp->prev;
+        } else {
+            tail = temp->prev; // Deleting the tail
+        }
         delete temp;
     }
-}
+    void print() {
+        Node* current = head;
+        if (!current) return;
+            while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+    void print_reverse() {
+        Node* current = tail;
+        if (!current) return;
+        while (current) {
+            cout << current->data << " ";
+            current = current->prev;
+        }
+        cout << endl;
+    }
+    ~DoublyLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
 };
+
+class Goat {
+    int age;
+    string name;
+    string color;
+    string names[15] = {}; // TODO populate array with random names
+    string colors[15] = {}; // TODO populate array with random colors
+
+    public:
+    // default constructor
+    Goat() {
+        /*
+        The default constructor will randomly assign:
+        an age, 1-20
+        a name, randomly selected from the 15-element names[] array
+        a color, randomly selected from the 15-element colors[] array
+        The parameter constructor will be a typical 3-element parameter setup.
+        */
+        srand(time(0)); // set the seed for rand()
+        age = (rand() % (AGE_MIN - AGE_MAX + 1)) + AGE_MIN;
+        // TODO set the name by randomly selecting from names array
+
+        // TODO set the name by randomly selecting from colors array
+
+    }
+};
+
 // Driver program
 int main() {
     DoublyLinkedList list;

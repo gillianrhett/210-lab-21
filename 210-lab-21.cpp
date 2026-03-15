9b5de6620784f06ee6a5eba2b752576f7d317798
gillianrhett <gillianrhett@gmail.com>
2026-03-14 17:29:25 -0700

    mostly done, need to add input validation for age parameter

--- 210-lab-21.cpp
+++ 210-lab-21.cpp
@@ -7,6 +7,47 @@ using namespace std;
 const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20; // constants from the original file
 const int NAMES_SIZE = 15, COLORS_SIZE = 15, AGE_MIN = 1, AGE_MAX = 20; // my new constants for Goat class
 
+class Goat {
+    int age;
+    string name;
+    string color;
+    string names[NAMES_SIZE] {"Craig", "Katina", "Mike", "Blair", "Lorene", "Sol", "Ken", "Tony",
+                        "Haley", "Leslie", "Harrison", "Tyler", "Carlene", "Sanford", "Whitney"};
+    string colors[COLORS_SIZE] = {"light brown", "red", "light gray", "medium gray", "dark gray", 
+                        "black", "white", "medium brown", "dark brown", "tan", "yellow", "orange",
+                        "spotted", "striped", "beige",}; 
+
+    public:
+    // default constructor
+    Goat() {
+        /*
+        The default constructor will randomly assign:
+        an age, 1-20
+        a name, randomly selected from the 15-element names[] array
+        a color, randomly selected from the 15-element colors[] array
+        The parameter constructor will be a typical 3-element parameter setup.
+        */
+        age = (rand() % (AGE_MIN - AGE_MAX + 1)) + AGE_MIN;
+        // set the name by randomly selecting from names array
+        int i = (rand() % (NAMES_SIZE));
+        name = names[i];
+        // set the color by randomly selecting from colors array
+        i = (rand() % (COLORS_SIZE));
+        color = colors[i];
+    }
+
+    // parameter constructor
+    Goat(int a, string n, string c) {
+        age = a;
+        name = n;
+        color = c;
+    }
+
+    string getName() { return name; }
+    int getAge() { return age; }
+    string getColor() { return color; }
+};
+
 class DoublyLinkedList {
     private:
     struct Node {
@@ -98,20 +139,31 @@ class DoublyLinkedList {
     
     void print() {
         Node* current = head;
-        if (!current) return;
-            while (current) {
-            // TODO display the data on each goat
-
+        if (!current) {
+            cout << "The list is empty.";
+            return;
+        }
+        cout << endl;
+        while (current) {
+            cout << "\t" << current->data.getName() << "(" << 
+            current->data.getColor() << ", " <<
+            current->data.getAge() << ")" << endl;
             current = current->next;
         }
         cout << endl;
     }
+
     void print_reverse() {
         Node* current = tail;
-        if (!current) return;
+        if (!current) {
+            cout << "The list is empty.";
+            return;
+        }
+        cout << endl;
         while (current) {
-            // TODO display the data on each goat
-            
+            cout << "\t" << current->data.getName() << "(" << 
+            current->data.getColor() << ", " <<
+            current->data.getAge() << ")" << endl;
             current = current->prev;
         }
         cout << endl;
@@ -125,68 +177,32 @@ class DoublyLinkedList {
     }
 };
 
-class Goat {
-    int age;
-    string name;
-    string color;
-    string names[NAMES_SIZE] {"Craig", "Katina", "Mike", "Blair", "Lorene", "Sol", "Ken", "Tony",
-                        "Haley", "Leslie", "Harrison", "Tyler", "Carlene", "Sanford", "Whitney"};
-    string colors[COLORS_SIZE] = {"light brown", "red", "light gray", "medium gray", "dark gray", 
-                        "black", "white", "medium brown", "dark brown", "tan", "yellow", "orange",
-                        "spotted", "striped", "beige",}; 
-
-    public:
-    // default constructor
-    Goat() {
-        /*
-        The default constructor will randomly assign:
-        an age, 1-20
-        a name, randomly selected from the 15-element names[] array
-        a color, randomly selected from the 15-element colors[] array
-        The parameter constructor will be a typical 3-element parameter setup.
-        */
-        age = (rand() % (AGE_MIN - AGE_MAX + 1)) + AGE_MIN;
-        // set the name by randomly selecting from names array
-        int i = (rand() % (NAMES_SIZE));
-        name = names[i];
-        // set the color by randomly selecting from colors array
-        i = (rand() % (COLORS_SIZE));
-        color = colors[i];
-    }
-
-    // parameter constructor
-    Goat(int a, string n, string c) {
-        age = a;
-        name = n;
-        color = c;
-    }
-
-    string getName() { return name; };
-};
-
 // Driver program
 int main() {
     srand(time(0)); // set the seed for rand()
     /*
-    _ Task. Modify the DoublyLinkedList class's push_front() and push_back() functions such 
+    x Task. Modify the DoublyLinkedList class's push_front() and push_back() functions such 
     that it has a Goat object as a parameter, rather than an int. 
 
     _ Task. In main(), create a DoublyLinkedList object. Append to it a random number of Goat 
     objects, range 5-20 or so.
 
-    _ Task. In main(), call your methods to print both forward and backward to show the proper 
+    x Task. In main(), call your methods to print both forward and backward to show the proper 
     traversals.
 
-    _ Task. Use srand(time(0)); as one of your first lines in main() to randomize your random 
+    x Task. Use srand(time(0)); as one of your first lines in main() to randomize your random 
     numbers.
 
-    _ Task. Update both of the print() methods that are in the class. Both should display the 
+    x Task. Update both of the print() methods that are in the class. Both should display the 
     text "List is empty" if the list is empty; otherwise, output the list as shown in the sample output below.
     */
     DoublyLinkedList list;
     int size = rand() % (MAX_LS-MIN_LS+1) + MIN_LS;
     // TODO create 5-20 goats and append them to the list
-
+    Goat goat1;
+    list.push_back(goat1);
+    Goat goat2;
+    list.push_back(goat2);
     cout << "List forward: ";
     list.print();
     cout << "List backward: ";
